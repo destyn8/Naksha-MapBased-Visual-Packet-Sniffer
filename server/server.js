39f2c258c1cc9f576f6453ae1d0d2c91c023afc9
Desfolio <64887065+pointAcross@users.noreply.github.com>
2024-06-19 19:53:10 +0530

    Add files via upload

--- server/server.js
+++ server/server.js
@@ -11,9 +11,29 @@ app.get('/',(req,res)=>{
 sockio.on('connection',(socket)=>{
 	console.log('connection recvd');
 	socket.on('newPacket',(data)=>{
-		console.log("message recvd[server]");
+		console.log("Message recvd[server]");
 		sockio.emit('newPacket',data);
 	});
+	socket.on('packetLen',(data)=>{
+		console.log("Length recvd[server]");
+		sockio.emit('packetLen',data);
+	});
+	socket.on('packetProt',(data)=>{
+		console.log("Protocol recvd[server]");
+		sockio.emit('packetProt',data);
+	});
+	socket.on('packetTTL',(data)=>{
+		console.log("TTL recvd[server]");
+		sockio.emit('packetTTL',data);
+	});
+    socket.on('interProt',function(data){
+        console.log("IP recvd[server]");
+		sockio.emit('interProt',data);
+    });
+    socket.once('packetHome',(data)=>{
+        console.log("origin recvd[server]");
+		sockio.emit('packetHome',data);    	
+    });
 	socket.on('disconnect',()=>{
 		console.log('user disconnected');
 	});
